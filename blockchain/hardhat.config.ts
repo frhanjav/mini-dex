b7718af711a520e529d1147a006293568a987ff6
frhanjav <farhan.javed006@gmail.com>
2025-08-15 20:20:01 +0530

    final-contract

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,33 @@
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      mining: {
+        auto: true,
+        interval: 0,
+      },
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
+};
+
+export default config;
